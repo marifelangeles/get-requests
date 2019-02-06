f2e49f97f16d68871505477c6cb9675eef482a04
Marifel Angeles <mmarifel@gmail.com>
2019-02-06 16:06:53 -0600

    add client side

---
+++ server/public/client.js
@@ -0,0 +1,17 @@
+
+
+$(document).ready(docReady);
+
+function docReady() {
+    
+    // display quote in table
+    $('#quoteTableBody').append(`
+                    <tr>
+                        <td>
+                            Remember there's no such thing as a small act of kindness. Every act creates a ripple with no logical end.
+                        </td>
+                        <td>Scott Adams</td>
+                    </tr>
+                    `);
+    
+} // end docReady
